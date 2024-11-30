b8eef04770345e7a2c83507a7b31a0a02c9b81c0
malascoding <asepabdulmugni22@gmail.com>
2024-11-30 21:41:53 +0700

    modified about store

--- src/stores/about.js
+++ src/stores/about.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { defineStore } from "pinia";
 
 export const useAboutStore = defineStore("about", {
@@ -7,11 +8,96 @@ export const useAboutStore = defineStore("about", {
     loadingButton: false,
     linkImage: "http://localhost/hepytech-api/public/storage/img/",
     userName: localStorage.getItem("name"),
+
+    //form update
+    formUpdate: false,
+
+    // DATA ABOUT DETAIL
+    dataAboutDetail: null,
+    dataAboutFilter: null,
+
+    // DATA FOR FORM UPDATE
+    imageForm: null,
   }),
   getters: {
     initialUserName() {
       return this.userName.substring(0, 1).toUpperCase();
     },
   },
-  actions: {},
+  actions: {
+    getAboutDetail() {
+      axios({
+        method: "get",
+        url: "http://localhost:8000/api/about",
+      })
+        .then((response) => {
+          this.dataAboutDetail = response.data.data[0];
+          // salin data about ke  data about filter
+          this.dataAboutFilter = { ...this.dataAboutDetail };
+          this.dataAboutFilter.description = this.textFilter();
+        })
+        .catch((error) => {
+          swal({
+            icon: "error",
+            title: "Error",
+            text: "Please check your connections",
+          });
+        })
+        .finally(() => {
+          this.loadingTable = false;
+        });
+    },
+    textFilter() {
+      const limiText = 200;
+      if (this.dataAboutFilter.description.length > limiText) {
+        return this.dataAboutFilter.description.substring(0, limiText) + "...";
+      } else {
+        return this.dataAboutFilter.description;
+      }
+    },
+    updateToDatabase(id) {
+      this.loadingButton = true;
+      axios({
+        method: "post",
+        url: "http://localhost:8000/api/about/" + id + "/update",
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+          "Content-Type": "multipart/form-data",
+        },
+        data: {
+          clients: this.dataAboutDetail.clients,
+          project: this.dataAboutDetail.project,
+          staff: this.dataAboutDetail.staff,
+          description: this.dataAboutDetail.description,
+          image: this.imageForm,
+          _method: "put",
+        },
+      })
+        .then((response) => {
+          swal({
+            icon: "success",
+            title: "Success update",
+          }).then((isTrue) => {
+            if (isTrue) {
+              this.formUpdate = false;
+              this.dataAboutDetail = response.data.data;
+              this.dataAboutFilter = { ...this.dataAboutDetail };
+              this.dataAboutFilter.description = this.textFilter();
+            }
+          });
+        })
+        .catch((error) => {
+          swal({
+            icon: "error",
+            title: "Error, Please Try again later",
+          });
+        })
+        .finally(() => {
+          this.loadingButton = false;
+        });
+    },
+    imageChange(e) {
+      this.imageForm = e.target.files[0];
+    },
+  },
 });
--- src/views/AdminAboutDetail.vue
+++ src/views/AdminAboutDetail.vue
@@ -3,8 +3,101 @@
   <Sidebar />
   <!-- Edn Sidebar  -->
 
+  <!-- FORM UPDATE -->
+  <section
+    v-if="useAboutStore().formUpdate == true"
+    class="fixed bg-slate-600 bg-opacity-50 flex items-center justify-center w-full h-screen z-50"
+  >
+    <form
+      data-aos="zoom-in"
+      @submit.prevent="
+        useAboutStore().updateToDatabase(useAboutStore().dataAboutDetail.id)
+      "
+      class="container w-11/12 md:w-4/12 h-5/6 relative bg-white rounded-xl shadow-lg p-5 overflow-y-scroll scrollbar-hide border-2 border-slate-400"
+    >
+      <h1
+        class="mx-auto font-semibold text-xl w-full text-center mb-5 text-slate-800"
+      >
+        UPDATE ABOUT
+      </h1>
+      <div class="w-full py-2">
+        <label for="" class="text-slate-700 font-semibold">
+          Clients
+        </label>
+        <input
+          class="w-full border outline-none rounded-md p-1 shadow-md"
+          type="number"
+          placeholder="999"
+          v-model="useAboutStore().dataAboutDetail.clients"
+        />
+      </div>
+      <div class="w-full py-2">
+        <label for="" class="text-slate-700 font-semibold">
+          Project
+        </label>
+        <input
+          class="w-full border outline-none rounded-md p-1 shadow-md"
+          type="number"
+          placeholder="999"
+          v-model="useAboutStore().dataAboutDetail.project"
+        />
+      </div>
+      <div class="w-full py-2">
+        <label for="" class="text-slate-700 font-semibold">
+          Staff
+        </label>
+        <input
+          class="w-full border outline-none rounded-md p-1 shadow-md"
+          type="number"
+          placeholder="999"
+          v-model="useAboutStore().dataAboutDetail.staff"
+        />
+      </div>
+      <div class="w-full py-2">
+        <label for="image" class="font-semibold text-slate-700">
+          Image
+        </label>
+        <input
+          class="w-full border outline-none rounded-md p-1 shadow-md"
+          type="file"
+          id="image"
+          @change="useAboutStore().imageChange($event)"
+        />
+      </div>
+      <div class="w-full py-2">
+        <label for="description" class="font-semibold text-slate-700">
+          Description
+        </label>
+        <textarea
+          class="w-full border rounded-lg shadow-md p-1 outline-none"
+          id="description"
+          rows="8"
+          v-model="useAboutStore().dataAboutDetail.description"
+        ></textarea>
+      </div>
+      <div class="w-full py-2 flex gap-5">
+        <button
+          class="py-2 px-5 bg-purple-600 rounded-lg shadow-md text-white font-semibold"
+          :disabled="useAboutStore().loadingButton"
+        >
+          <span v-if="useAboutStore().loadingButton == false">Update</span>
+          <span v-if="useAboutStore().loadingButton == true">Loading</span>
+        </button>
+        <button
+          class="py-2 px-5 bg-slate-500 text-white border rounded-lg shadow-md font-semibold"
+          @click="useAboutStore().formUpdate = false"
+        >
+          Cancel
+        </button>
+      </div>
+    </form>
+  </section>
+  <!-- END FORM UPDATE  -->
+
   <!-- MAIN -->
-  <main class="w-full min-h-screen flex flex-wrap bg-slate-100">
+  <main
+    class="w-full min-h-screen flex flex-wrap bg-gradient-to-br from-red-50 via-slate-50 to-yellow-50"
+  >
     <!-- HEADER  -->
     <AdminHeading
       :name="useAboutStore().userName"
@@ -16,7 +109,7 @@
     <section class="min-h-screen w-10/12 pt-20 ms-auto px-5">
       <!-- title  -->
       <h1 class="font-bold text-2xl text-slate-800 text-center my-5">
-        Home
+        About
       </h1>
       <!-- title  -->
 
@@ -27,27 +120,65 @@
           <!-- table heading  -->
           <tr>
             <th class="w-3/12 py-5 border-b">Image</th>
-            <th class="w-2/12 py-5 border-b">Title</th>
-            <th class="w-2/12 py-5 border-b">Description</th>
-            <th class="w-2/12 py-5 border-b">Link Contac us</th>
-            <th class="w-2/12 py-5 border-b">Link Job</th>
+            <th class="w-3/12 py-5 border-b">Description</th>
+            <th class="w-2/12 py-5 border-b">Clients</th>
+            <th class="w-2/12 py-5 border-b">Project</th>
+            <th class="w-1/12 py-5 border-b">Staff</th>
             <th class="w-1/12 py-5 border-b">Action</th>
           </tr>
           <!-- end table heading  -->
+          <!-- data  -->
+          <tr v-if="useAboutStore().dataAboutFilter != null">
+            <!-- image  -->
+            <td class="text-center pt-5">
+              <img
+                :src="`${useAboutStore().linkImage}${
+                  useAboutStore().dataAboutDetail.image
+                }`"
+                alt=""
+              />
+            </td>
+            <!-- end imgae  -->
+            <!-- Description  -->
+            <td class="px-2">
+              <span class="text-sm">
+                {{ useAboutStore().dataAboutFilter.description }}
+              </span>
+            </td>
+            <!-- End Description  -->
+            <td class="text-center">
+              <span>{{ useAboutStore().dataAboutFilter.clients }}</span>
+            </td>
+            <td class="text-center">
+              <span>{{ useAboutStore().dataAboutFilter.project }}</span>
+            </td>
+            <td class="text-center">
+              <span>{{ useAboutStore().dataAboutFilter.staff }}</span>
+            </td>
+            <td class="text-center">
+              <button
+                @click="useAboutStore().formUpdate = true"
+                class="py-1 px-2 bg-purple-600 text-white rounded-lg shadow-md"
+              >
+                UPDATE
+              </button>
+            </td>
+          </tr>
+          <!-- end data  -->
           <!-- loading  -->
-          <!-- <tr v-if="useHomeStore().loadingTable == true">
+          <tr v-if="useAboutStore().loadingTable == true">
             <th colspan="7" class="text-blue-600 pt-5 text-xl">Loading...</th>
-          </tr> -->
+          </tr>
           <!-- end loading  -->
           <!-- no data  -->
-          <!-- <tr
+          <tr
             v-if="
-              useHomeStore().loadingTable == false &&
-              useHomeStore().dataHomeDetail == null
+              useAboutStore().loadingTable == false &&
+              useAboutStore().dataAboutDetail == null
             "
           >
             <th class="pt-5 text-red-500 text-xl" colspan="7">No Data</th>
-          </tr> -->
+          </tr>
           <!-- end no data  -->
         </table>
         <!-- table news -->
@@ -73,6 +204,9 @@ export default {
     return {
       useAboutStore,
     };
+  },
+  mounted() {
+    useAboutStore().getAboutDetail();
   },
 };
 </script>
