8e7a983c7fe559104b1389fb2febf25d123d2af9
malascoding <asepabdulmugni22@gmail.com>
2024-12-01 09:55:15 +0700

    add function show detail contac

--- src/stores/contac.js
+++ src/stores/contac.js
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import axios from "axios";
 
 export const useContacStore = defineStore("contac", {
   state: () => ({
@@ -6,11 +7,36 @@ export const useContacStore = defineStore("contac", {
     loadingTable: true,
     loadingButton: false,
     userName: localStorage.getItem("name"),
+
+    formUpdate: false,
+
+    // DATA CONTAC
+    contacDetail: null,
   }),
   getters: {
     initialUserName() {
       return this.userName.substring(0, 1).toUpperCase();
     },
   },
-  actions: {},
+  actions: {
+    getContacDetail() {
+      axios({
+        method: "get",
+        url: "http://localhost:8000/api/contac",
+      })
+        .then((response) => {
+          this.contacDetail = response.data.data[0];
+        })
+        .catch((error) => {
+          swal({
+            icon: "error",
+            title: "Error",
+            text: "Please check your connections",
+          });
+        })
+        .finally(() => {
+          this.loadingTable = false;
+        });
+    },
+  },
 });
--- src/views/AdminContac.vue
+++ src/views/AdminContac.vue
@@ -35,21 +35,55 @@
           </tr>
           <!-- end table heading  -->
           <!-- data  -->
+          <tr v-if="useContacStore().contacDetail != null">
+            <td class="text-center pt-5">
+              <span>{{ useContacStore().contacDetail.facebook }}</span>
+            </td>
+            <td class="text-center pt-5">
+              <span>{{ useContacStore().contacDetail.instagram }}</span>
+            </td>
+            <td class="text-center pt-5">
+              <span>{{ useContacStore().contacDetail.x }}</span>
+            </td>
+            <td class="text-center pt-5">
+              <button
+                @click="
+                  useTrustedStore().dataFormUpdate(index, item.id, item.link)
+                "
+                class="p-2 text-white rounded-lg shadow-md hover:bg-purple-800 bg-purple-600"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  class="fill-current"
+                >
+                  <path
+                    d="m7 17.013 4.413-.015 9.632-9.54c.378-.378.586-.88.586-1.414s-.208-1.036-.586-1.414l-1.586-1.586c-.756-.756-2.075-.752-2.825-.003L7 12.583v4.43zM18.045 4.458l1.589 1.583-1.597 1.582-1.586-1.585 1.594-1.58zM9 13.417l6.03-5.973 1.586 1.586-6.029 5.971L9 15.006v-1.589z"
+                  ></path>
+                  <path
+                    d="M5 21h14c1.103 0 2-.897 2-2v-8.668l-2 2V19H8.158c-.026 0-.053.01-.079.01-.033 0-.066-.009-.1-.01H5V5h6.847l2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2z"
+                  ></path>
+                </svg>
+              </button>
+            </td>
+          </tr>
           <!-- end data  -->
           <!-- loading  -->
-          <!-- <tr v-if="useAboutStore().loadingTable == true">
+          <tr v-if="useContacStore().loadingTable == true">
             <th colspan="7" class="text-blue-600 pt-5 text-xl">Loading...</th>
-          </tr> -->
+          </tr>
           <!-- end loading  -->
           <!-- no data  -->
-          <!-- <tr
+          <tr
             v-if="
-              useAboutStore().loadingTable == false &&
-              useAboutStore().dataAboutDetail == null
+              useContacStore().loadingTable == false &&
+              useContacStore().contacDetail == null
             "
           >
             <th class="pt-5 text-red-500 text-xl" colspan="7">No Data</th>
-          </tr> -->
+          </tr>
           <!-- end no data  -->
         </table>
         <!-- table news -->
@@ -75,6 +109,9 @@ export default {
     return {
       useContacStore,
     };
+  },
+  mounted() {
+    useContacStore().getContacDetail();
   },
 };
 </script>
