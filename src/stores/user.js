f5fb47e478e252280eea59e9488f3c3bfda5f766
malascoding <asepabdulmugni22@gmail.com>
2024-12-04 10:36:01 +0700

    add form add user

--- src/stores/user.js
+++ src/stores/user.js
@@ -3,8 +3,16 @@ import axios from "axios";
 
 export const useUserStore = defineStore("user", {
   state: () => ({
+    formAdd: false,
+    loadingButton: false,
     loadingTable: true,
     listUser: [],
+
+    //data form add
+    nameAdd: null,
+    emailAdd: null,
+    passwordAdd: null,
+    role_idAdd: null,
   }),
   getters: {},
   actions: {
@@ -24,6 +32,31 @@ export const useUserStore = defineStore("user", {
         })
         .finally(() => {
           this.loadingTable = false;
+        });
+    },
+    createUser() {
+      this.loadingButton = true;
+      axios({
+        method: "post",
+        url: "http://localhost:8000/api/user/store",
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+        data: {
+          name: this.nameAdd,
+          email: this.emailAdd,
+          password: this.passwordAdd,
+          role_id: this.role_idAdd,
+        },
+      })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          this.loadingButton = false;
         });
     },
   },
--- src/views/Admin/UserView.vue
+++ src/views/Admin/UserView.vue
@@ -7,6 +7,95 @@
   <AdminHeading :name="userName" :initialName="initialName" />
   <!-- edn HEADING  -->
 
+  <!-- FORM CREATE -->
+  <section
+    v-if="useUserStore().formAdd == true"
+    class="fixed bg-slate-600 bg-opacity-50 flex items-center justify-center w-full h-screen z-50"
+  >
+    <form
+      data-aos="zoom-in"
+      @submit.prevent="useUserStore().createUser()"
+      class="container w-11/12 md:w-4/12 h-5/6 relative bg-white rounded-xl shadow-lg p-5 overflow-y-scroll scrollbar-hide border-2 border-slate-400"
+    >
+      <!-- title  -->
+      <h1
+        class="mx-auto font-semibold text-xl w-full text-center mb-5 text-slate-800"
+      >
+        CREATE USER
+      </h1>
+      <!-- end title  -->
+      <!-- input  -->
+      <div class="w-full py-2">
+        <label class="text-slate-700 font-semibold">
+          Name
+        </label>
+        <input
+          class="w-full border outline-none rounded-md p-1 shadow-md"
+          type="text"
+          placeholder="Zharif Khaleed"
+          max="100"
+          v-model="useUserStore().nameAdd"
+          required
+        />
+      </div>
+      <div class="w-full py-2">
+        <label class="text-slate-700 font-semibold">
+          Email
+        </label>
+        <input
+          class="w-full border outline-none rounded-md p-1 shadow-md"
+          type="email"
+          placeholder="kingzharif@gmail.com"
+          v-model="useUserStore().emailAdd"
+          required
+        />
+      </div>
+      <div class="w-full py-2">
+        <label class="text-slate-700 font-semibold">
+          Password
+        </label>
+        <input
+          class="w-full border outline-none rounded-md p-1 shadow-md"
+          type="text"
+          placeholder="zharif27234@#"
+          v-model="useUserStore().passwordAdd"
+          required
+        />
+      </div>
+      <div class="w-full py-2">
+        <label class="text-slate-700 font-semibold">
+          Role
+        </label>
+        <select
+          v-model="useUserStore().role_idAdd"
+          class="w-full border p-1 shadow-md rounded-md outline-none"
+          required
+        >
+          <option value="1">Admin</option>
+        </select>
+      </div>
+      <!-- edn input  -->
+      <!-- box btn  -->
+      <div class="w-full py-2 flex gap-5">
+        <button
+          class="py-2 px-5 bg-purple-600 rounded-lg shadow-md text-white font-semibold"
+          :disabled="useUserStore().loadingButton"
+        >
+          <span v-if="useUserStore().loadingButton == false">Create</span>
+          <span v-if="useUserStore().loadingButton == true">Loading</span>
+        </button>
+        <button
+          class="py-2 px-5 bg-slate-500 text-white border rounded-lg shadow-md font-semibold"
+          @click="useUserStore().formAdd = false"
+        >
+          Cancel
+        </button>
+      </div>
+      <!-- edn box btn  -->
+    </form>
+  </section>
+  <!-- END FORM CREATE  -->
+
   <!-- SECTION OF CONTENT  -->
   <section
     class="min-h-screen w-10/12 bg-gradient-to-br from-red-50 via-slate-100 to-yellow-50 ms-auto pt-20 px-5"
@@ -21,6 +110,7 @@
     <div class="bg-white p-5 rounded-lg shadow-lg">
       <!-- btn add  -->
       <button
+        @click="useUserStore().formAdd = true"
         class="mb-5 px-5 py-2 bg-green-600 hover:bg-green-800 active:ring-2 ring-green-500 rounded-lg text-white font-semibold shadow-md"
       >
         Add User
